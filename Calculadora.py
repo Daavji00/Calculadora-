4646fd8d195d6f33c909d8a941612e08f09c7994
Daavji00 <avilajimenezdavid@gmail.com>
2022-07-31 19:28:28 +0200

    Panel de salida incluido y comando cuando se pulsa botón

--- Calculadora.py
+++ Calculadora.py
@@ -7,32 +7,46 @@ import tkinter as tk
 
 from pyparsing import col
 
+# Funcion reconocer expresion
+
+def expresion(exp) :
+    output.insert(0, str(exp))
+
+
+
 # Main 
 
 window = tk.Tk() #Crear ventana principal 
 
 '''
+  Salida
+'''
+output = tk.Text(window, height=2)
+output.grid(row=4, column=1)
+
+
+'''
   Agregar botones a la interfaz del uno al diez
 '''
-zero_button = tk.Button(window, text='0', width=5, height=2)
+zero_button = tk.Button(window, text='0', width=5, height=2, command= lambda: expresion(0))
 zero_button.grid(row=3, column=1)
-one_button = tk.Button(window, text='1', width=5, height=2) 
+one_button = tk.Button(window, text='1', width=5, height=2, command= lambda: expresion(1)) 
 one_button.grid(row=2, column=0)
-two_button = tk.Button(window, text='2', width=5, height=2)
+two_button = tk.Button(window, text='2', width=5, height=2, command= lambda: expresion(2))
 two_button.grid(row=2, column=1)
-three_button = tk.Button(window, text='3', width=5, height=2)
+three_button = tk.Button(window, text='3', width=5, height=2, command= lambda: expresion(3))
 three_button.grid(row=2, column=2)
-four_button = tk.Button(window, text='4', width=5, height=2)
+four_button = tk.Button(window, text='4', width=5, height=2, command= lambda: expresion(4))
 four_button.grid(row=1,column=0)
-five_button = tk.Button(window, text='5', width=5, height=2)
+five_button = tk.Button(window, text='5', width=5, height=2, command= lambda: expresion(5))
 five_button.grid(row=1,column=1)
-six_button = tk.Button(window, text='6', width=5, height=2)
+six_button = tk.Button(window, text='6', width=5, height=2, command= lambda: expresion(6))
 six_button.grid(row=1,column=2)
-seven_button = tk.Button(window, text='7', width=5, height=2)
+seven_button = tk.Button(window, text='7', width=5, height=2, command= lambda: expresion(7))
 seven_button.grid(row=0,column=0)
-eight_button = tk.Button(window, text='8', width=5, height=2)
+eight_button = tk.Button(window, text='8', width=5, height=2, command= lambda: expresion(8))
 eight_button.grid(row=0,column=1)
-nine_button = tk.Button(window, text='9', width=5, height=2)
+nine_button = tk.Button(window, text='9', width=5, height=2, command= lambda: expresion(9))
 nine_button.grid(row=0,column=2)
 
 '''
@@ -62,4 +76,7 @@ ac_button.grid(row=0, column=3, columnspan=2)
 eq_button = tk.Button(window, text='=', width=5, height=2)
 eq_button.grid(row=0, column=2)
 
+
+
 window.mainloop() # Correr ventana principal
+
