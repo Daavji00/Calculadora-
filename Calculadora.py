aabd0a78a0f6c8972b7c7e35921c06b6b143380a
Daavji00 <avilajimenezdavid@gmail.com>
2022-08-10 03:05:08 +0200

    Mostrar y borrar expresiones separado en dos funciones

--- Calculadora.py
+++ Calculadora.py
@@ -4,31 +4,30 @@
 
 import tkinter as tk
 
-# Funcion reconocer expresion
+# Funcion reconocer showExp
 
-def expresion(exp) :
-    if exp == 'AC' : # Borramos y mostramos 0
-      output.delete(1.0,'end')
-      output.insert(1.0, '0')
-      updatePosText(exp)
-    
-    elif exp == 'BC': #Borramos una posicion de la pantalla
-      updatePosText(exp)
-      if posText == 0 :
-        output.delete(1.0)
-        output.insert(1.0, '0')
-      else :
-        output.delete('1.' + str(posText))
-    
-    elif posText == 0 : # Cambiamos valor 0 por otro valor
+def showExp(exp) :  
+  if posText == 0 : # Cambiamos valor 0 por otro valor
+    output.delete(1.0)
+    output.insert(1.0, str(exp))
+    updatePosText('IN')
+  elif posText < 30 : # Mostramos los numeros en orden al que se marcan
+    output.insert('1.' + str(posText), str(exp))
+    updatePosText('IN')
+
+def deleteExp(action) :
+  if action == 'AC' : # Borramos y mostramos 0
+    output.delete(1.0,'end')
+    output.insert(1.0, '0')
+    updatePosText(action)  
+  elif action == 'BC': #Borramos una posicion de la pantalla
+    updatePosText(action)
+    if posText == 0 :
       output.delete(1.0)
-      output.insert(1.0, str(exp))
-      updatePosText('IN')
-
-    else : # Mostramos los numeros en orden al que se marcan
-      if posText < 30 : 
-        output.insert('1.' + str(posText) , str(exp))
-        updatePosText('IN')
+      output.insert(1.0, '0')
+    else :
+      output.delete('1.' + str(posText))
+    
 
 def updatePosText(accion):
   global posText
@@ -61,25 +60,25 @@ output.insert(1.0, "0") # Insertar valor inicial 0 en la salida
 frame_botones = tk.Frame(window)
 frame_botones.grid(row=1, column=0)
 
-zero_button = tk.Button(frame_botones, text='0', width=5, height=2, command= lambda: expresion(0))
+zero_button = tk.Button(frame_botones, text='0', width=5, height=2, command= lambda: showExp(0))
 zero_button.grid(row=3, column=1)
-one_button = tk.Button(frame_botones, text='1', width=5, height=2, command= lambda: expresion(1)) 
+one_button = tk.Button(frame_botones, text='1', width=5, height=2, command= lambda: showExp(1)) 
 one_button.grid(row=2, column=0)
-two_button = tk.Button(frame_botones, text='2', width=5, height=2, command= lambda: expresion(2))
+two_button = tk.Button(frame_botones, text='2', width=5, height=2, command= lambda: showExp(2))
 two_button.grid(row=2, column=1)
-three_button = tk.Button(frame_botones, text='3', width=5, height=2, command= lambda: expresion(3))
+three_button = tk.Button(frame_botones, text='3', width=5, height=2, command= lambda: showExp(3))
 three_button.grid(row=2, column=2)
-four_button = tk.Button(frame_botones, text='4', width=5, height=2, command= lambda: expresion(4))
+four_button = tk.Button(frame_botones, text='4', width=5, height=2, command= lambda: showExp(4))
 four_button.grid(row=1,column=0)
-five_button = tk.Button(frame_botones, text='5', width=5, height=2, command= lambda: expresion(5))
+five_button = tk.Button(frame_botones, text='5', width=5, height=2, command= lambda: showExp(5))
 five_button.grid(row=1,column=1)
-six_button = tk.Button(frame_botones, text='6', width=5, height=2, command= lambda: expresion(6))
+six_button = tk.Button(frame_botones, text='6', width=5, height=2, command= lambda: showExp(6))
 six_button.grid(row=1,column=2)
-seven_button = tk.Button(frame_botones, text='7', width=5, height=2, command= lambda: expresion(7))
+seven_button = tk.Button(frame_botones, text='7', width=5, height=2, command= lambda: showExp(7))
 seven_button.grid(row=0,column=0)
-eight_button = tk.Button(frame_botones, text='8', width=5, height=2, command= lambda: expresion(8))
+eight_button = tk.Button(frame_botones, text='8', width=5, height=2, command= lambda: showExp(8))
 eight_button.grid(row=0,column=1)
-nine_button = tk.Button(frame_botones, text='9', width=5, height=2, command= lambda: expresion(9))
+nine_button = tk.Button(frame_botones, text='9', width=5, height=2, command= lambda: showExp(9))
 nine_button.grid(row=0,column=2)
 
 '''
@@ -99,14 +98,14 @@ sub_button.grid(row=2, column=4)
   Agregar boton de all clear
 '''
 
-ac_button = tk.Button(frame_botones, text='AC', width=5, height=2, command= lambda: expresion('AC'))
+ac_button = tk.Button(frame_botones, text='AC', width=5, height=2, command= lambda: deleteExp('AC'))
 ac_button.grid(row=0, column=4)
 
 '''
   Boton borrar un parámetro
 '''
 
-bc_button = tk.Button(frame_botones, text='BC', width=5, height=2, command=lambda: expresion('BC'))
+bc_button = tk.Button(frame_botones, text='BC', width=5, height=2, command=lambda: deleteExp('BC'))
 bc_button.grid(row=0, column=3)
 
 '''
